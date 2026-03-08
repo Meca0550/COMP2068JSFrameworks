37446cec98c97297cea2aa1c1798d790efcab800
Meca0550 <dhakalbijay0550@gmail.com>
2026-03-08 18:44:43 -0400

    init commit

---
+++ LAB04/sushiapp/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <main>
+            <h1>About this site</h1>
+            <p>We are building this site for com2061 in winter 2026</p>
+        </main>
+    );
+}
---
+++ LAB04/sushiapp/app/sushi/page.tsx
@@ -0,0 +1,7 @@
+export default function Sushi(){
+    return(
+        <main>
+            <h1>Our sushi menu</h1>
+        </main>
+    );
+}
